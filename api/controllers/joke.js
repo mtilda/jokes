f3ade5d4bad9391e4fa797164a6ed5021e577a90
mxkay <codingwithkay@gmail.com>
2021-01-10 00:24:37 -0500

    Finish vote route

--- api/controllers/joke.js
+++ api/controllers/joke.js
@@ -19,20 +19,25 @@ joke.get('/random', async (req, res) => {
 joke.put('/vote', async (req, res) => {
   console.log(req.body);
   if (!req.body.vote) res.status(400).json('no vote detected');
-  try {
-    // find joke
-    const joke = Joke.find({ setup: req.body.setup });
-    // if joke exists
-    if (joke) {
-      // add the vote to it
-      const newJoke = Joke.findByIdAndUpdate(joke.id, { ...joke, votes: [...joke.votes, req.body.vote] });
+  else {
+    try {
+      // find joke
+      const joke = Joke.find({ setup: req.body.setup });
+      // if joke exists
+      if (joke) {
+        // add the vote to it
+        const newJoke = Joke.findByIdAndUpdate(joke.id, { ...joke, votes: [...joke.votes, req.body.vote] });
+        res.status(200).json(newJoke);
+      } else {
+        // create the joke and add the vote
+        const newJoke = Joke.create({ ...req.body, vote: [req.body.vote] });
+        res.status(200).json(newJoke);
+      }
+      console.log(joke);
+      res.status(200).json({});
+    } catch (error) {
+      res.status(400).json(error);
     }
-    // else
-    // create the joke and add the vote
-    console.log(joke);
-    res.status(200).json({});
-  } catch (error) {
-    res.status(400).json(error);
   }
 });
 
