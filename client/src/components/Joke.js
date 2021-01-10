e51c9878b028b1ce8a0d5c761e2027f9237b8a5f
mxkay <codingwithkay@gmail.com>
2021-01-10 01:35:26 -0500

    Fix styled components warnings

--- client/src/components/Joke.js
+++ client/src/components/Joke.js
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+const Setup = styled.h1``;
+const Punchline = styled.h2`
+  width: fit-content;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: ${({ isRevealed }) => isRevealed ? 'none' : 'black'};
+  color: #E6D0EE;
+  user-select: none;
+`;
+
 export default ({ setup, punchline }) => {
   const [isRevealed, setIsRevealed] = useState(false);
 
   useEffect(() => setIsRevealed(false), [setup, punchline]);
-
-  const Setup = styled.h1``;
-  const Punchline = styled.h2`
-    width: fit-content;
-    padding: 10px;
-    border-radius: 10px;
-    background-color: ${isRevealed ? 'none' : 'black'};
-    color: #E6D0EE;
-    user-select: none;
-  `;
 
   return (
     <>
       {/* <label for='setup' className='sr-only'>setup</label> */}
       <Setup id='setup'>{setup}</Setup>
       <br aria-hidden='true' />
-      <label for='punchline' className='sr-only'>punchline</label>
+      <label htmlFor='punchline' className='sr-only'>punchline</label>
       <Punchline
         id='punchline'
         onClick={() => setIsRevealed(true)}
@@ -29,5 +29,4 @@ export default ({ setup, punchline }) => {
       </Punchline>
     </>
   );
-}
-;
+};
--- client/src/components/VoteButton.js
+++ client/src/components/VoteButton.js
@@ -4,27 +4,30 @@ import {
   MdThumbDown as ThumbDown
 } from 'react-icons/md';
 
+const Button = styled.button`
+  width: 2em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: ${({ isUp }) => isUp ? '#B7E1CD' : '#F5BC8D'};
+  font-size: 4rem;
+
+  :hover {
+    background-color: ${({ isUp }) => isUp ? '#6BAA8C' : '#F48024'};
+  }
+`;
+
 export default ({ up, down, handleVote }) => {
   // default to up (true) if none or both are specified
   const isUp = up || !down;
 
-  const Button = styled.button`
-    width: 2em;
-    height: 2em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    background-color: ${isUp ? '#B7E1CD' : '#F5BC8D'};
-    font-size: 4rem;
-
-    :hover {
-      background-color: ${isUp ? '#6BAA8C' : '#F48024'};
-    }
-  `;
-
   return (
-    <Button onClick={() => handleVote(isUp ? 'up' : 'down')}>
+    <Button
+      isUp={isUp}
+      onClick={() => handleVote(isUp ? 'up' : 'down')}
+    >
       {
         isUp
           ? <ThumbUp />
